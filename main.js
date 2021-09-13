72f886a37e8b092dd72760135e261bcabffbc37f
Armin Sayar <armin@m.lo.agency>
2021-09-13 13:09:38 +0430

    feat
    JS file updated.
    - Click events added for profile menu and location section

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 'use strict'
 
+// Profile menu
+
+$('#profile-btn').click(function () {
+    $('.nav-menu-js').toggleClass('dis-bl');
+    $('.location-js').hasClass('dis-bl') ?
+        $('.location-js').removeClass('dis-bl')
+        : pass;
+
+})
+
+// Location
+
+$('.btn-before').click(function () {
+    $('.location-js').toggleClass('dis-bl');
+    $('.nav-menu-js').hasClass('dis-bl') ?
+        $('.nav-menu-js').removeClass('dis-bl')
+        : pass;
+})
+
+// Closing clicks
+
+$(document).click(function (e) {
+    if
+        (
+        !$(e.target).closest('#profile-btn').length
+        && !$(e.target).closest('.btn-before').length
+        && !$(e.target).closest('.location-js').length
+    ) {
+        $('.nav-menu-js').removeClass('dis-bl');
+        $('.location-js').removeClass('dis-bl');
+    }
+})
