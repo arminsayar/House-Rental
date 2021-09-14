5022f5ff801842817ee5939a6d5e1b8664aeed6e
Armin Sayar <armin@m.lo.agency>
2021-09-14 13:33:08 +0430

    feat
    lang list added with JS

--- main.js
+++ main.js
@@ -23,9 +23,30 @@ $('.btn-before').click(function () {
 
 $("button[name='pricelist']").click(function () {
     $('.price-list').toggleClass('price-list-on');
+    $('.price-list').hasClass('price-list-on') ?
+        $('body').css('overflow-y', 'hidden')
+        : null;
+
 })
+
 $("svg[name='closebtn-p']").click(function () {
     $('.price-list').toggleClass('price-list-on');
+    $('body').css('overflow-y', 'auto');
+})
+
+// Lang list
+
+$("button[name='langlist']").click(function () {
+    $('.lang-list').toggleClass('lang-list-on');
+    $('.lang-list').hasClass('lang-list-on') ?
+        $('body').css('overflow-y', 'hidden')
+        : null;
+
+})
+
+$("svg[name='closebtn-l']").click(function () {
+    $('.lang-list').toggleClass('lang-list-on');
+    $('body').css('overflow-y', 'auto');
 })
 
 // Closing clicks
@@ -38,9 +59,18 @@ $(document).click(function (e) {
         && !$(e.target).closest('.location-js').length
         && !$(e.target).closest('.price-list').length
         && !$(e.target).closest("button[name='pricelist']").length
+        && !$(e.target).closest("button[name='langlist']").length
     ) {
         $('.nav-menu-js').removeClass('dis-bl');
         $('.location-js').removeClass('dis-bl');
         $('.price-list').removeClass('price-list-on');
+        $('.lang-list').removeClass('lang-list-on');
+        $('body').css('overflow-y', 'auto');
     }
+})
+
+// Body overflow-x && DOM fully loaded events
+
+$(document).ready(function () {
+    $('body').css('overflow-x', 'hidden')
 })
